d245d5fe40db64933bf4899507f7aed97d3aeac0
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-07-05 15:33:13 +0700

    disable github font script
    by far my laziest script

---
+++ src/github/disableFont.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Disable GitHub Font
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Sets the font family in GitHub to "sans-serif", allowing for a custom font to be set
+// @author       You
+// @match        https://github.com/
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+document.body.style.fontFamily = "sans-serif";

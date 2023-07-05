67c056932ec6214a28384b7421899330e6d9fc6c
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-07-05 15:39:47 +0700

    fixed match url

--- src/github/disableFont.js
+++ src/github/disableFont.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  Sets the font family in GitHub to "sans-serif", allowing for a custom font to be set
 // @author       You
-// @match        https://github.com/
+// @match        https://github.com/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
